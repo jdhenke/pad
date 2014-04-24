b1fc059c328ccd82e31d8eb687ef49aed8bc57d5
Evan Wang <vagrant@precise64.(none)>
2014-04-24 05:56:38 +0000

    Removed extraneous code

--- nworker/app.js
+++ nworker/app.js
@@ -11,9 +11,6 @@ var rebase = require('./routes/rebase');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
